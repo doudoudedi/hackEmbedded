6d3c289900578e7f4bb770b4de27903121895e4b
doudoudedi <52043932+doudoudedi@users.noreply.github.com>
2022-05-09 15:40:05 +0800

    Add files via upload

---
+++ source/hackebds_for-pip-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+filepath="./README.md"
+readme_data=open(filepath).read()
+
+
+setup(name='hackebds',
+      version='0.0.8',
+      description='This tool is used for backdoor and shellcode generation for various architecture devices',
+      long_description=readme_data,
+      long_description_content_type="text/markdown",
+      url='https://github.com/doudoudedi/hackEmbedded',
+      author='doudoudedi',
+      author_email='doudoudedi233@gmail.com',
+      license='MIT',
+      py_modules=['hackebds.arm','hackebds.mips',"hackebds.aarch64","hackebds.extract_shellcode"],
+      data_files=[filepath]
+)
+
